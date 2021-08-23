225647ae5d90c7c7d823b6f37af2a44030ecebdb
AmanPachori <amangirrajpachori2002@gmail.com>
2021-08-23 16:21:05 +0530

    finished

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const body = document.querySelector("body");
 window.addEventListener("load", () => {
   body.classList.add("visible");
 });
+
+const signIn = document.querySelector(".sign-in-sign-up");
+
+signIn.addEventListener("click", () => {
+  location.href = "/pages/dashbord/dashboard.html";
+});
---
+++ pages/sign-in/index.js
@@ -0,0 +1,73 @@
+const body = document.querySelector("body");
+
+const url = "http://localhost:5000";
+
+const signup = document.querySelector(".form-signup");
+
+signup.addEventListener("submit", () => {
+  event.preventDefault();
+  const email = document.querySelector(".email-signup").value;
+  const name = document.querySelector(".entername").value;
+  const password = document.querySelector(".password-signup").value;
+  const repassword = document.querySelector(".repassword").value;
+  console.log(email, name, password, repassword);
+
+  if (password !== repassword) {
+    window.alert("password does not match");
+    return;
+  }
+
+  fetch(`${url}/auth/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const { token } = data;
+
+      if (token) {
+        localStorage.setItem("jwt", token);
+        location.href = "/pages/dashboard/dashboard.html";
+      } else {
+        alert("SignUp Again");
+      }
+    })
+    .catch((err) => {
+      alert("Error Signing Up!!! Re-try....");
+      console.log(err);
+    });
+});
+
+const signin = document.querySelector(".form-signIn");
+
+signin.addEventListener("submit", () => {
+  event.preventDefault();
+  const email = document.querySelector(".email").value;
+  const password = document.querySelector(".password").value;
+
+  fetch(`${url}/auth/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const { token } = data;
+
+      if (token) {
+        localStorage.setItem("jwt", token);
+        location.href = "/pages/dashbord/dashboard.html";
+      } else {
+        alert("SignIn Again");
+      }
+    })
+    .catch((err) => {
+      alert("Error Signing In!!! Re-try....");
+      console.log(err);
+    });
+});
